f1090df98051cbe0e8b2046399df9e1f2caa2662
Adrisson Galvao <adrisson36@gmail.com>
2018-03-12 00:14:47 -0400

    init projects with pandas

---
+++ treating_data_frame.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+
+def treating_csv():
+    df = pd.read_csv('./data_test.csv')
+    df.drop('InvoiceNo',axis=1,inplace=True)
+    df = df[(df['Quantity'] > 0)]
+    return df
+
+def product_more_sale_country(df,country):
+    df_country =pd.DataFrame(df[(df['Country'] ==country)])
+    df_country.drop(['InvoiceDate','UnitPrice','CustomerID','Country','StockCode'],axis=1,inplace=True)
+    list_quantity=df_country.groupby('Quantity').sum()
+    return list_quantity.max()
+
+df_main=treating_csv()
+print(product_more_sale_country(df_main,"United Kingdom"))
