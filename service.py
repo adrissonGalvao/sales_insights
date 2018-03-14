854a3b37637e05e060ce2db9e00b18c3ec4fd631
Adrisson Galvao <adrisson36@gmail.com>
2018-03-14 17:52:56 -0400

    new feature best_clint in service

--- service.py
+++ service.py
@@ -10,13 +10,21 @@ class Service:
         product_more_sale = insights.product_more_sale_country(self.df_main,product)
         json_data = json.dumps(product_more_sale, indent=4, separators=(',', ' : '), sort_keys=True)
         return json_data
+    
+    def best_client(self):
+        best_client= insights.best_client(df_main)
 
+        best_client['bigger_quantity']['value_sale']=str(best_client['bigger_quantity']['value_sale'])
+        best_client['bigger_price']['value_sale']=str(best_client['bigger_price']['value_sale'])
         
+        json_data = json.dumps(best_client,indent=4, separators=(',', ' : '), sort_keys=True)
+        return json_data
+
 service=Service()
 df_main =service.df_main 
-print(service.product_more_sale_country("United Kingdom"))
+#print(service.product_more_sale_country("United Kingdom"))
 #print(insights.product_more_sale(df_main))
-#print(insights.best_client(df_main))
+print(service.best_client())
 #print(insights.price_max_and_min(df_main,'RED TOADSTOOL LED NIGHT LIGHT'))
 #print(insights.percentage_product(df_main,'RED TOADSTOOL LED NIGHT LIGHT'))
 
